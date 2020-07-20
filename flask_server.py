3a8f60ccca1f1450c5def5e52d6926147b89d44e
Benoit GIELLY <benoit.gielly@gmail.com>
2020-07-20 11:45:51 +0100

    local update

--- flask_server.py
+++ flask_server.py
@@ -1,87 +1,105 @@
+"""Download songs from Pandora using a Flask server.
+
+:created:
+:author:
+
+"""
+from __future__ import print_function
+
 import os
 import re
 import urllib
 
-from flask import Flask, request, jsonify
-
+from flask import Flask, jsonify, request
 from mutagen.mp4 import MP4, MP4Cover
 
 # =============================================================================
-# Config
+# config
 # =============================================================================
+# modify These lines to change where the songs are saved
+ROOT_FOLDER = r"D:\musics\Pandora"
 
-# Modify These lines to change where the songs are saved
-base_path = os.sep.join(( os.path.expanduser("~"), 'Music', 'Pandora' ))
-
-# Possible Variables - Mix and match as desired
+# possible Variables - Mix and match as desired
 # {station} {artist} {album} {title}
-save_template = os.sep.join((
-    "{station}",
-    "{artist}",
-    "{album} - {title}.m4a"
-    # "{title}.m4a"
-)).encode('utf-8')
+FILE_TEMPLATE = os.sep.join(
+    (
+        "{station}",
+        "{artist}",
+        "{title}.m4a",
+        # "{album} - {title}.m4a"
+    )
+)
 
 # =============================================================================
+APP = Flask("Pandora Downloader Server")
 
-app = Flask( "Pandora Downloader Server" )
 
-@app.route( '/download', methods=['POST'] )
-def pandoraDownloader():
+@APP.route("/download", methods=["POST"])
+def pandora_downloader():
+    """Download songs from Pandora, Woop Woop."""
+    # playlist path
+    playlist_path = os.sep.join(
+        [
+            ROOT_FOLDER,
+            re.sub(r'[<>\*:\\/"|?]', "", request.form["station"]) + ".m3u",
+        ]
+    )
 
-    playlist_path = os.sep.join((
-        base_path,
-        re.sub(u'[<>\*:\\/\"|?]', '', request.form['station'].encode('utf-8') ) + ".m3u"
-    ))
+    # build the song's path - remove any invalid characters
+    relative_song_path = FILE_TEMPLATE.format(
+        station=re.sub(r'[<>\*:\\/"|?]', "", request.form["station"]),
+        artist=re.sub(r'[<>\*:\\/"|?]', "", request.form["artist"]),
+        album=re.sub(r'[<>\*:\\/"|?]', "", request.form["album"]),
+        title=re.sub(r'[<>\*:\\/"|?]', "", request.form["title"]),
+    )
 
-    # Build the song's path - remove any invalid characters
-    relative_song_path = save_template.format(
-        station = re.sub(u'[<>\*:\\/\"|?]', '', request.form['station'].encode('utf-8') ),
-        artist  = re.sub(u'[<>\*:\\/\"|?]', '', request.form['artist' ].encode('utf-8') ),
-        album   = re.sub(u'[<>\*:\\/\"|?]', '', request.form['album'  ].encode('utf-8') ),
-        title   = re.sub(u'[<>\*:\\/\"|?]', '', request.form['title'  ].encode('utf-8') )
-    ).decode('utf-8')
-
-    song_path = os.sep.join(( base_path, relative_song_path ))
+    song_path = os.sep.join([ROOT_FOLDER, relative_song_path])
 
     # check to see if the file has been downloaded before!
-    if os.path.exists( song_path ):
-        print( 'Song found already' )
-        return jsonify( status = 'alreadyDownloaded' )
-
+    if os.path.exists(song_path):
+        print("Song found already")
+        return jsonify(status="alreadyDownloaded")
     else:
+        print("Downloading {!r}".format(request.form["title"]))
 
-        print( 'Downloading "' + request.form['title'].encode('utf-8') + '"' )
+        # create the directories if they don't exist
+        if not os.path.isdir(os.path.split(song_path)[0]):
+            os.makedirs(os.path.split(song_path)[0])
 
-        # Create the directories if they don't exist
-        if not os.path.isdir( os.path.split(song_path)[0] ): os.makedirs( os.path.split(song_path)[0] )
+        # download the song
+        try:
+            urllib.urlretrieve(request.form["url"], song_path)
+        except BaseException:
+            print("Something went wrong... skipping this song!")
+            return jsonify(status="failed")
 
-        # Download the song
-        urllib.urlretrieve( request.form['url'], song_path )
+        # open the song with mutagen so we can tag it
+        # and put the album art in it
+        try:
+            song = MP4(song_path)
+        except BaseException:
+            print("Something went wrong... skipping this song!")
+            return jsonify(status="failed")
 
-        # Open the song with mutagen so we can tag it and put the album art in it
-        song = MP4( song_path )
+        # set the tags
+        song["\xa9nam"] = [request.form["title"]]
+        song["\xa9ART"] = [request.form["artist"]]
+        song["\xa9alb"] = [request.form["album"]]
 
-        # Set the tags
-        song['\xa9nam'] = [ request.form['title'].encode('utf-8')  ]
-        song['\xa9ART'] = [ request.form['artist'].encode('utf-8') ]
-        song['\xa9alb'] = [ request.form['album'].encode('utf-8')  ]
-
-        # Download the album art and put it in the file
-        albumArtRequest = urllib.urlopen( request.form['albumArt'] )
-        albumArt = MP4Cover( albumArtRequest.read() )
-        albumArtRequest.close()
-
-        song['covr'] = [ albumArt ]
-
+        # download the album art and put it in the file
+        album_art_request = urllib.urlopen(request.form["albumArt"])
+        album_art = MP4Cover(album_art_request.read())
+        album_art_request.close()
+        song["covr"] = [album_art]
         song.save()
 
-        # Append the song in the playlist
-        with open( playlist_path, 'a+' ) as playlist:
-            playlist.write(( relative_song_path + "\n" ).encode("utf-8"))
-        
-        print( 'Download Complete!' )
-        return jsonify( status = 'success' )
+        # append the song in the playlist
+        with open(playlist_path, "a+") as playlist:
+            playlist.write((relative_song_path + "\n"))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        print("Download Complete!")
+        return jsonify(status="success")
+
+
+if __name__ == "__main__":
+    APP.run(debug=True)
